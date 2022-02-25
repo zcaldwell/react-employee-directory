a3d5af244397a5afb58f4fd9181976ace13361f7
zcaldwell <mintyst.john@gmail.com>
2022-02-25 09:45:44 -0800

    privateroute created

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { UserProvider } from './context/UserContext/UserContext';
 import Auth from './views/Auth/Auth';
 import ConfirmEmail from './views/Auth/ConfirmEmail';
 import Home from './views/Home/Home';
-
+import Header from './components/Header/Header';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Profile from './components/Profile/Profile';
 export default function App() {
   return (
     <UserProvider>
+      <Header />
       <BrowserRouter>
         <Switch>
           <Route path="/login">
@@ -18,6 +21,9 @@ export default function App() {
           <Route path="/confirm-email">
             <ConfirmEmail />
           </Route>
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
           <Route>
             <Home />
           </Route>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { useUser } from '../../context/UserContext/UserContext';
-import { Link } from 'react-router-dom';
 import AuthButton from '../AuthButton/AuthButton';
 
 export default function Header() {
-  const { user, setUser } = useUser();
+  const { user } = useUser();
 
   return (
     <>
       <h2>Employee Directory</h2>
       {user?.email ? (
-        <h3>Welcome {user}!</h3>
+        <h3>Welcome {user?.email}!</h3>
       ) : (
         <>
           <h3>Please Sign In</h3>
           <AuthButton />
         </>
       )}
-      )
     </>
   );
 }

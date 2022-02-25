94de6b4d39935e38aa7c8ba2720cd5173ce22d9c
zcaldwell <mintyst.john@gmail.com>
2022-02-24 17:17:30 -0800

    auth written

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -3,6 +3,42 @@ import { Link, useHistory } from 'react-router-dom';
 import { signInUser, signUpUser } from '../../services/users';
 import { useUser } from '../../context/UserContext/UserContext';
 
-export default function Auth({}) {
-  return <div>Auth</div>;
+export default function Auth({ isSigningUp = false }) {
+  const { setUser } = useUser();
+  const history = useHistory();
+
+  const handleAuth = async (email, password) => {
+    try {
+      if (isSigningUp) {
+        await signUpUser(email, password);
+        history.push('/confirm-email');
+      } else {
+        const response = await signInUser(email, password);
+        setUser({ id: response.id, email: response.email });
+        history.replace('/profile');
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return (
+    <main>
+      <h2>{isSigningUp ? 'Welcome!' : 'Welcome Back'}</h2>
+      <UserForm
+        onSubmit={handleAuth}
+        label={isSigningUp ? 'Sign Up' : 'Sign In'}
+      />
+
+      {isSigningUp ? (
+        <p>
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      ) : (
+        <p>
+          Need to Sign-up? <Link to="/register"></Link>
+        </p>
+      )}
+    </main>
+  );
 }

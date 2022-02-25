611b4ea8de4dfc7e0a95458b2537a27cd5642001
zcaldwell <mintyst.john@gmail.com>
2022-02-24 16:24:01 -0800

    services written

---
+++ src/views/Auth/Auth.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { signInUser, signUpUser } from '../../services/users';
+import { useUser } from '../../context/UserContext/UserContext';
+
+export default function Auth({}) {
+  return <div>Auth</div>;
+}

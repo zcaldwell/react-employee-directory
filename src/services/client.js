1db3a6f0f47b55e01ca139ee30fd5b00c15f4eed
zcaldwell <mintyst.john@gmail.com>
2022-02-25 13:53:30 -0800

    test passing

--- src/services/client.js
+++ src/services/client.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const client = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
 );
 
 export const parseData = ({ data, error }) => {
